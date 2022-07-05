395775102d01ecea2f3c5c25d90c30319b8c164d
JimmyChang <jimmychang@nine-yi.com>
2022-07-05 17:15:02 +0800

    chore: 測試 transation

--- RedisTutorial/Program.cs
+++ RedisTutorial/Program.cs
@@ -11,29 +11,30 @@ var lockFactory = new LockFactory(connectionMutiplexer.GetConnection());
 // Create Cache Provider and Service
 ICacheProvider redisCacheProvider = new RedisProvider(connectionMutiplexer.GetRedisDB(), 
                                                       lockFactory, 
-                                                      lockWay: CacheLib.Enum.LockWayEnum.RedLock);
+                                                      lockWay: CacheLib.Enum.LockWayEnum.RedisLock);
 ICacheService cacheService = new CacheService(redisCacheProvider);
 
 string testKey = "Test:Redis:Tutorial";
 string testKey2 = "Test:Redis:Tutorial2";
 
-//var setResult = cacheService.Set<string>(testKey, "1234");
-//var setResult2 = cacheService.Set<string>(testKey2, "5678");
+//var result = cacheService.StringBatchSet<string>(new List<KeyValuePair<string, string>>()
+//{
+//    new KeyValuePair<string, string>("Test1","1"),
+//    new KeyValuePair<string, string>("Test2","2"),
+//});
 
-var result = cacheService.StringBatchSet<string>(new List<KeyValuePair<string, string>>()
-{
-    new KeyValuePair<string, string>("Test1","1"),
-    new KeyValuePair<string, string>("Test2","2"),
-});
+var result1 = cacheService.SetByTransationAsync<string>(testKey, "Test3");
+var result2 = cacheService.SetByTransationAsync<string>(testKey2, "Test4");
+
 
 CacheEntity<string>? getResult = cacheService.GetWithExpire<string>(testKey);
 var getResult2 = cacheService.StringBatchGet(new List<string>() { "Test1", "Test2" });
 
-//List<string> testSet = new List<string>() { "Test", "Test1", "Test2" };
-//var testSetResult = cacheService.StringBatchGet(testSet);
+
+var b = cacheService.Set<Setting>("Test3", new Setting { Id = 1, Value = "Test"});
+var c = cacheService.GetWithExpire<Setting>("Test3");
 
 Console.WriteLine("Hello, World!");
-
 
 public class Setting
 {
